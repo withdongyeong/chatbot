dbe4981fbdbb2375b5d53aed20e09668074d2e8c
withdongyeong <withdongyeong@gmail.com>
2021-09-28 18:24:14 +0900

    fix toWeb.py

--- toWeb.py
+++ toWeb.py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 import chromedriver_autoinstaller
-import time
+from selenium.webdriver.chrome.options import Options
 
 def goDestination(target):
+    chrom_options = Options()
+    chrom_options.add_experimental_option("detach", True)
+
     url = "http://search.danawa.com/dsearch.php?k1=" + target + "&module=goods&act=dispMain"
     path = chromedriver_autoinstaller.install()
     driver = webdriver.Chrome(path)
     driver.get(url)
-
-    time.sleep(50)
-
-    driver.close()
-    driver.quit()
-
 
 if __name__ == '__main__':
     goDestination("gtx1060")
